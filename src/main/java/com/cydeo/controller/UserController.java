dc6c412e4751175e188399ccad45f3358bdf1cca
efecagrisen <efecagrisen@yahoo.com>
2023-12-23 09:57:43 +0300

    AOP added

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.cydeo.controller;
 
+import com.cydeo.annotation.ExecutionTime;
 import com.cydeo.dto.ResponseWrapper;
 import com.cydeo.dto.UserDTO;
 import com.cydeo.exeption.TicketingProjectException;
@@ -25,7 +26,7 @@ public class UserController {
         this.userService = userService;
     }
 
-
+    @ExecutionTime
     @GetMapping
     @RolesAllowed({"Manager","Admin"})
     @Operation(summary = "Get users")
@@ -34,7 +35,7 @@ public class UserController {
         return ResponseEntity.ok(new ResponseWrapper("Users are succesfully retrieved",userDTOList, HttpStatus.OK));
     }
 
-
+    @ExecutionTime
     @GetMapping("/{username}")
     @RolesAllowed({"Admin"})
     @Operation(summary = "Get user by username")
